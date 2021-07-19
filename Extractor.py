f475e113f65addb4fb0834414afd506f52fc3a73
ts482 <65360237+ts482@users.noreply.github.com>
2021-07-19 02:31:15 +0100

    Extra errors and marker changes
    Have added extra rules for errors, created a system to count marker chromosomes and introduced the ability to detect multi-chromosome translocation

--- Extractor.py
+++ Extractor.py
@@ -54,10 +54,11 @@ def properties_dict(karyotypes, properties = None):
             if v[-1] == v[0]:
                 v = v[1:-1]
         
-        #recognising monosomies
+        #Formatting monosomies
         if v.startswith('Monosomy'):
             v = '-' + v[8:]
-            
+        
+        #
         matches = re.finditer('(\d+)(p|q)',v)
         for m in matches:
             f = v[:m.start()] + '(' + m.group()[:-1] + \
@@ -67,8 +68,9 @@ def properties_dict(karyotypes, properties = None):
         #creating escape characters for strings
         v = re.escape(v)
         
+        #creating special case for for t(v;11)
         if v == 't\\(v;11\\)':
-            v = 't\\(\d+;11\\)'
+            v = '(t\\(\d+;11\\))|(t\\(11;\d+\\))'
         
         d[k] = v
     return {v:k for k,v in d.items()}
@@ -85,96 +87,192 @@ def remove_artefact(row):
             return row['Cytogenetics'].replace(' ', '')[:-7]
         return row['Cytogenetics'].replace(' ', '')
     return 'Error'
+
+
 def gram_error(string):
     '''
-    Recognise if there is a grammatical error within a karyotype report
+    Takes in a cytogenetic report string and checks for multiple error types.
+    Returns a list describing the errors picked up.
+    
+    Error types included:
+    1. Automated process failure (e.g. presence of 'Error' or 'fail')
+    2. Warnings in presence of constitutional changes
+    3. Deducible punctuation rules, usually to do with ratios of sections
+    4. Counting expected number of chromosomes and comparing to stated number
+    for each report section
     '''
+    error = []
+    
+    #basic report errors
     if string == 'Error':
-        return 'String report missing'
+        error.append('String report missing')
     if re.search('fail', string.lower()):
-        return 'String report indicates failure'
+        error.append('String report indicates failure')
+        
+    #all reports should include commas somewhere 
     if not re.search(',', string):
-        return 'Missing comma'
+        error.append('Missing comma')
+        
+    #checking for constitutional changes
+    if re.search('[^a-z]?c[^p]', string) or re.search('[^a-z]c$', string):
+        error.append('constitutional changes present')
+    
+    #grammatical rules regarding symbol ratios
     missing = list()
     for k in punct_dict:
         punct_dict[k] = len(re.findall(re.escape(k), string))
     if punct_dict['/'] != punct_dict['[']-1:
-        return 'incorrrect ratio of \'\\\' to \'[]\' '
+        error.append('incorrrect ratio of \'\\\' to \'[]\' ')
     if punct_dict['['] != punct_dict[']']:
         missing.append(min(['[', ']'], key=punct_dict.get))
     if punct_dict['('] != punct_dict[')']:
         missing.append(min(['(',')'], key=punct_dict.get))
     if len(missing)!=0:
-        return f'missing grammar: {", ".join(missing)}'
+        error.append(f'missing grammar: {", ".join(missing)}')
+        
+        
+    #checking number in subsections matches stated number
     substring = re.split('/', string)
     for i, s in enumerate(substring):
         try:
-            chrom = s[:s.index(',')] #chromsome string
+            chrom = s[:s.index(',')] #chromosome string
         except ValueError:
-            return 'Part of report missing comma'
+            error.append('Part of report missing comma')
         if not re.search('idem', s):
             expected = 46
         expected -= len(re.findall('\-', s))
         expected += len(re.findall('\+', s))
-        if re.search('[~]', s):
+        if re.search('mar', s):
+            mar_plural = re.search('\+(\d)(~\d)?mar',s)
+            if mar_plural:
+                expected += int(mar_plural.groups()[0]) - 1
+                tilda_present =  re.search('\+(\d)~(\d)mar', s)
+                #error if variable number of markers included (e.g. +2~4mar)
+                if tilda_present:
+                    d = tilda_present.group(1)
+                    error.append(f'Variable number of markers in report. Minimum number ({d}) used by default')
+        
+        #in the case of a range of potential chromosome counts
+        if re.search('[~]', chrom):
             try:
                 low_num, high_num = int(chrom[:2]), int(chrom[-2:])
             except ValueError:
-                return 'Part of report not clearly defined by two chromosome numbers followed by comma (e.g. "43~45,")'
+                error.append('Part of report not clearly defined by two chromosome numbers followed by comma (e.g. "43~45,")')
             if low_num <= expected <= high_num:
                 pass
-            elif expected < low_num:
-                return f'chromsome number lower than expected in {i+1} subsection'
+            elif expected > high_num:
+                error.append(f'chromosome number lower than expected in subsection number {i+1}')
             else:
-                return f'chromsome number higher than expected in {i+1} subsection'
+                error.append(f'chromosome number higher than expected in subsection number {i+1}')
+                
+        #if only one chromosome count is present
         else:
             try:
                 num = int(chrom[:2])
             except ValueError:
-                return 'Start of report missing clear chromosome number followed by comma (e.g. "46,")'
+                error.append('Start of report missing clear chromosome number followed by comma (e.g. "46,")')
             if expected == num:
                 pass
-            elif expected > num:
-                return f'chromsome number higher than expected in {i+1} subsection'
+            elif expected < num:
+                error.append(f'chromosome number higher than expected in subsection number {i+1}')
             else:
-                return f'chromsome number lower than expected in {i+1} subsection'
-    pass
+                error.append(f'chromosome number lower than expected in subsection number {i+1}')
+    return error
 
+def make_multi_translocation_dict(string):
+    '''
+    Creates a translocation list if only full chromosomes are involved
+    in translocation, or a dictionary if chromosome arms are involved
+    '''
+    
+    #finding out how many chromosomes in translocation
+    exp = '(\d\d?);(\d\d?)'
+    while re.search(exp, string):
+        chr_groups = list(re.search(exp, string).groups())
+        exp = exp + ';(\d\d?)'
+        
+    #adding arms if present
+    if re.search('[pq]', string):
+        exp2 = '([pq]\d\d?)'
+        for g in range(len(chr_groups)-1):
+            exp2 = exp2 + ';([pq]\d\d?)'
+        arm_groups = list(re.search(exp2, string).groups())
+        return {int(c):a for c,a in zip(chr_groups, arm_groups)}
+    return chr_groups
+
+def check_trans_dict(trans_dict, a):
+    '''
+    Takes in multi translocation dictionary and detects abnormalities associated
+    with adjacent chromosome translocation
+    '''
+    chr_keys = list(trans_dict)
+    for i in range(len(chr_keys)-1):
+        sorted_list = sorted([chr_keys[i],chr_keys[i+1]])
+        first_chr, second_chr = sorted_list[0], sorted_list[1]
+        for p in prop_dict:
+            if re.search(p, f't({first_chr};{second_chr})'):
+                col_true.add(p)
 
 def parse_karyotype(row, prop_dict):
     '''
-    Calculates which cytogenetic abnormalities are present in each report
-    and fills in the relevant columns
+    Working row by row on a dataframe, detects abnormalities
+    and fills in boolean and integer counts.
     '''
+    #checking for error
     error = gram_error(row['Cytogenetics'])
     if error:
-        row['Error'] = True
         row['Error description'] = error
-        if not error.startswith('chromsome'):
-            return row
+        #checking type of error is not permissible
+        for e in error:
+            if not e.startswith('chromosome'):
+                if not e.startswith('Variable'):
+                    if not e.startswith('constitutional'):
+                        row['Error'] = True
+                        return row
+    
+    #initialising counts and sets for abnormalities
     abnorms = set(re.split('/|,|\[', row['Cytogenetics']))
     removed = set()
     col_true = set()
     mono = 0
     struc = 0
     der = 0
+    mar = 0
     seventeen_p = False
     for a in abnorms:
-        if re.fullmatch('\d\d([~-]\d\d)?|X[XY]?|(cp)?\d\d?\]|idem', a):
+        #cremoving normal report segments
+        if re.fullmatch('(\d\d([~-]\d\d)?|X[XY]?|(cp)?\d\d?\]|idem)(\??c)?', a):
             removed.add(a)
 #         if re.search('mar', a): #no longer the case to remove mar
 #             removed.add(a)
-        
+
+        #checking abnormalities
         if a not in removed:
             
+            #checking multi-translocations
+            if re.search('t\(\d\d?;\d\d?;\d\d?',a):
+                trans_dict = make_multi_translocation_dict(a)
+                check_trans_dict(trans_dict, a)
+            
+            #counting number of markers present
+            if re.search('mar', a):
+                mar_plural = re.search('\+(\d)',a)
+                if mar_plural:
+                    mar += int(mar_plural.groups()[0])
+                else:
+                    mar += 1
+            
+            #detecting presence on monosomies
             if re.fullmatch('-\d+|-[XY]', a):
                 mono += 1
+            #detecting number of structular changes
             if re.search('[inv|t].*\).*\)', a):
                 struc += 1
             
             if re.search('der', a):
                 der += 1
-
+            
+            #working out if any abnormalities are to do with 17p
             if re.search('17.*p|-17',a):
                 m = re.search('\d\d?;\d\d?', a)
                 if m:
@@ -186,18 +284,21 @@ def parse_karyotype(row, prop_dict):
                 else:
                     seventeen_p = True
             
+            #looping through pre-set properties
             for p in prop_dict:
                 if re.search(p, a):
                     col_true.add(p)
+                    
+    #abnormality count is equal to all non-removed + der is double counted + mar count
     abnorms = abnorms.difference(removed)
-    row['Number of cytogenetic abnormalities'] = len(abnorms) + der
+    row['Number of cytogenetic abnormalities'] = len(abnorms) + der + mar
+    
     row['Monosomy'] = mono
     row['Structural'] = struc
     row['abnormal(17p)'] = seventeen_p
     for c in col_true:
         row[prop_dict[c]] = True
     return row
-
 
 
 if __name__ == '__main__':
